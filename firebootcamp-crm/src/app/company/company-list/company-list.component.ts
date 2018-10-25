86cf223a4372f5dc68f32f7c6c8549322c0c80f1
Neil Dobson <ndobson@barnardos.org.au>
2018-10-25 12:07:31 +1100

    Scaffold component

--- firebootcamp-crm/src/app/company/company-list/company-list.component.ts
+++ firebootcamp-crm/src/app/company/company-list/company-list.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-list.component.scss']
 })
 export class CompanyListComponent implements OnInit {
+  companies: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.companies = this.getCompanies();
   }
 
+  getCompanies() {
+    return [
+      { name: 'Company A', phone: '0401 234632', email: 'some@where.com' },
+      { name: 'Company B', phone: '0406 345363', email: 'who@where.com' },
+      { name: 'Company C', phone: '0602 634983', email: 'me@where.com' },
+      { name: 'Company D', phone: '0832 346943', email: 'they@where.com' },
+    ];
+  }
 }
