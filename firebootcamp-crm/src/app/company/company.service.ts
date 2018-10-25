558d4bd2089575d3bf679c3e7bdf545da2cc9189
Neil Dobson <ndobson@barnardos.org.au>
2018-10-25 15:04:39 +1100

    Add error handler

--- firebootcamp-crm/src/app/company/company.service.ts
+++ firebootcamp-crm/src/app/company/company.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@angular/core";
 import { Company } from "./company";
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+import { catchError } from "rxjs/operators";
+
 
 @Injectable({ providedIn: "root" })
 export class CompanyService {
@@ -10,6 +12,13 @@ export class CompanyService {
   constructor(private httpClient: HttpClient) {}
 
   getCompanies(): Observable<Company[]> {
-    return this.httpClient.get<Company[]>(`${this.API_BASE}/company`);
+    return this.httpClient
+      .get<Company[]>(`${this.API_BASE}/company`)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  private errorHandler(error: Error): Observable<Company[]> {
+    console.error("implement custom errort handler here", error);
+    return new Observable<Company[]>();
   }
 }
