9f4bb2f20f237b028828d5ce204516951fc1a386
Neil Dobson <ndobson@barnardos.org.au>
2018-10-25 11:56:35 +1100

    Added component: ng generate component company/company-list

---
+++ firebootcamp-crm/src/app/company/company-list/company-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fbc-company-list',
+  templateUrl: './company-list.component.html',
+  styleUrls: ['./company-list.component.scss']
+})
+export class CompanyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
