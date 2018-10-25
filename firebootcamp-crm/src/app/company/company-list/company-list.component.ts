33613916659cb2a2a82af386109926c47292744c
Neil Dobson <ndobson@barnardos.org.au>
2018-10-25 14:36:49 +1100

    Add tslint rule and clean-up

--- firebootcamp-crm/src/app/company/company-list/company-list.component.ts
+++ firebootcamp-crm/src/app/company/company-list/company-list.component.ts
@@ -1,26 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Company } from '../company';
+import { Component, OnInit } from "@angular/core";
+import { Company } from "../company";
+import { CompanyService } from "../company.service";
 
 @Component({
-  selector: 'fbc-company-list',
-  templateUrl: './company-list.component.html',
-  styleUrls: ['./company-list.component.scss']
+  selector: "fbc-company-list",
+  templateUrl: "./company-list.component.html",
+  styleUrls: ["./company-list.component.scss"]
 })
 export class CompanyListComponent implements OnInit {
   companies: Company[];
 
-  constructor() {}
+  constructor(private companyService: CompanyService) {}
 
   ngOnInit() {
     this.getCompanies();
   }
 
   getCompanies() {
-    this.companies = [
-      { name: 'Company A', phone: 401234632, email: 'some@where.com' },
-      { name: 'Company B', phone: 406345363, email: 'who@where.com' },
-      { name: 'Company C', phone: 602634983, email: 'me@where.com' },
-      { name: 'Company D', phone: 832346943, email: 'they@where.com' }
-    ];
+    this.companies = this.companyService.getCompanies();
   }
 }
--- firebootcamp-crm/src/app/company/company.service.ts
+++ firebootcamp-crm/src/app/company/company.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Company } from "./company";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: "root"})
 export class CompanyService {
+  constructor() {}
 
-  constructor() { }
+  getCompanies(): Company[] {
+    return [
+      { name: "Company A", phone: 401234632, email: "some@where.com" },
+      { name: "Company B", phone: 406345363, email: "who@where.com" },
+      { name: "Company C", phone: 602634983, email: "me@where.com" },
+      { name: "Company D", phone: 832346943, email: "they@where.com" }
+    ];
+  }
 }
