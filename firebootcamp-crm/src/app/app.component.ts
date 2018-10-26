75479b737468030b4ae8abf639f6d92f1bbbca0e
Neil Dobson <ndobson@barnardos.org.au>
2018-10-26 12:07:02 +1100

    Show number of records

--- firebootcamp-crm/src/app/app.component.ts
+++ firebootcamp-crm/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { CompanyService } from "./company/company.service";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'fbc-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "fbc-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'firebootcamp-crm';
+export class AppComponent implements OnInit {
+  prodMode: boolean;
+  companyCount$: Observable<number>;
+  title = "firebootcamp-crm";
+
+  constructor(
+    private companyService: CompanyService
+  ) { }
+
+  ngOnInit(): void {
+    this.prodMode = environment.production;
+    this.companyCount$ = this.companyService.getCompanies().pipe(map(c => c.length))
+  }
 }
